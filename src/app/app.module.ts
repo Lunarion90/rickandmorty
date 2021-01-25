b1c9de296705095ea1676143b09ba6185e3becce
Lunarion <tamas.kerekgyarto@hotmail.com>
2021-01-25 16:30:09 +0100

    Restructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainMenuComponent } from './main-menu/main-menu.component';
-import { CharListComponent } from './char-list/char-list.component';
-import { CharDetailsComponent } from './char-details/char-details.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { CharListComponent } from './components/char-list/char-list.component';
+import { CharDetailsComponent } from './components/char-details/char-details.component';
 
 @NgModule({
   declarations: [
