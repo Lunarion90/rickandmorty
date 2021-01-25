4d427226e5362841e09b6f0433779541853820fb
Lunarion <tamas.kerekgyarto@hotmail.com>
2021-01-25 15:55:33 +0100

    Landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { CharListComponent } from './char-list/char-list.component';
+import { CharDetailsComponent } from './char-details/char-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    CharListComponent,
+    CharDetailsComponent
   ],
   imports: [
     BrowserModule,
